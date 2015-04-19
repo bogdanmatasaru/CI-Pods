e049d868ba6bdbcd429a25277f4d9dd63bcc67c5
Fabrizzio <marco_fabrizzio@yahoo.com>
2015-04-19 18:12:36 +0300

    PUSH

--- CI-Pods/ViewController.m
+++ CI-Pods/ViewController.m
@@ -19,6 +19,37 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)pushMe:(id)sender {
+    
+    UIAlertController * alert=   [UIAlertController
+                                  alertControllerWithTitle:@"CI"
+                                  message:@"Hello Jenkins!"
+                                  preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* ok = [UIAlertAction
+                         actionWithTitle:@"OK"
+                         style:UIAlertActionStyleDefault
+                         handler:^(UIAlertAction * action)
+                         {
+                             [alert dismissViewControllerAnimated:YES completion:nil];
+                             
+                         }];
+    UIAlertAction* cancel = [UIAlertAction
+                             actionWithTitle:@"Cancel"
+                             style:UIAlertActionStyleDefault
+                             handler:^(UIAlertAction * action)
+                             {
+                                 [alert dismissViewControllerAnimated:YES completion:nil];
+                                 
+                             }];
+    
+    [alert addAction:ok];
+    [alert addAction:cancel];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
